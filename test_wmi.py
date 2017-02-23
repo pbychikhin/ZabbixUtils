dc13047d1ea48ae3d9b539c6810c26d47afe6dc7
Pavel Bychikhin <pbychikhin@yahoo.com>
2017-02-23 20:43:02 +0200

    test_wmi.py - working example how to use WMI. Gets IIS web sites info using WMI and DCOM

---
+++ test_wmi.py
@@ -0,0 +1,11 @@
+
+import wmi
+
+wobj = wmi.WMI(namespace="root/WebAdministration")
+counter = 0
+for site in wobj.instances("Site"):
+	print(counter,site.Name)
+	for binding in site.Bindings:
+		print("   for the protocol {} have the binding info {}".format(binding.protocol, binding.BindingInformation))
+	counter += 1
+
