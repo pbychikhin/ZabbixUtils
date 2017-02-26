c9e4c361a05d8af901f934a3ddca0a58fc965795
Pavel Bychikhin <pbychikhin@yahoo.com>
2017-02-26 20:41:18 +0200

    deleted test_wmi.py - no longer needed since we have a working script which uses code from the test

--- test_wmi.py
+++
@@ -1,9 +0,0 @@
-import wmi
-
-wobj = wmi.WMI(namespace="root/WebAdministration")
-counter = 0
-for site in wobj.instances("Site"):
-    print(counter, site.Name)
-    for binding in site.Bindings:
-        print("   for the protocol {} have the binding info {}".format(binding.protocol, binding.BindingInformation))
-    counter += 1
