2e69bb448ef6441056ddf3adfdd6a27c49abc354
Pavel Bychikhin <pbychikhin@yahoo.com>
2017-03-09 18:03:55 +0200

    header and body parsing

--- zabbix_item_IIS_site_http_probe.py
+++ zabbix_item_IIS_site_http_probe.py
@@ -6,6 +6,7 @@ import urllib.parse as up
 import re
 from argparse import ArgumentParser
 from ldap3.utils.ciDict import CaseInsensitiveDict as cidict
+from cgi import parse_header
 
 
 cmd = ArgumentParser(description="Probes a Web site over http(s)")
@@ -14,6 +15,7 @@ cmd.add_argument("-host", help="Host name", default="localhost")
 cmd.add_argument("-port", help="Optional TCP port number", default=None)
 cmd.add_argument("-addr", help="Optional IP address to connect to", default=None)
 cmd.add_argument("-path", help="Path component of the URL", default="")
+cmd.add_argument("-body", help="Regexp to search for in body", default=None)
 cmd.add_argument("-timeout", help="Operation timeout (default is 5m)", default=300)
 cmd.add_argument("-nameservers", help="Comma separated list of name servers", default=None)
 cmd.add_argument("-4", help="Resolve names to IPv4 only", dest="v4", action="store_true", default=False)
@@ -49,25 +51,29 @@ class Website:
 w = Website(args.scheme, args.host, args.port, args.addr, args.path)
 buffer = io.BytesIO()
 c = pycurl.Curl()
-c.setopt(c.URL, w.get_url())
+c.setopt(pycurl.URL, w.get_url())
 if args.v4 ^ args.v6:
     if args.v4:
-        c.setopt(c.IPRESOLVE, c.IPRESOLVE_V4)
+        c.setopt(pycurl.IPRESOLVE, pycurl.IPRESOLVE_V4)
     elif args.v6:
-        c.setopt(c.IPRESOLVE, c.IPRESOLVE_V6)
+        c.setopt(pycurl.IPRESOLVE, pycurl.IPRESOLVE_V6)
 else:
-    c.setopt(c.IPRESOLVE, c.IPRESOLVE_WHATEVER)
+    c.setopt(pycurl.IPRESOLVE, pycurl.IPRESOLVE_WHATEVER)
 if args.nameservers:
-    c.setopt(c.DNS_SERVERS, args.nameservers)
+    c.setopt(pycurl.DNS_SERVERS, args.nameservers)
 if w.get_curl_host() is not None:
-    c.setopt(c.RESOLVE, [w.get_curl_host()])
+    c.setopt(pycurl.RESOLVE, [w.get_curl_host()])
 if args.ca is not None:
-    c.setopt(c.CAINFO, args.ca)
-c.setopt(c.TIMEOUT, args.timeout)
-c.setopt(c.WRITEDATA, buffer)
-c.setopt(c.VERBOSE, args.verbose)
+    c.setopt(pycurl.CAINFO, args.ca)
+c.setopt(pycurl.TIMEOUT, args.timeout)
+c.setopt(pycurl.WRITEDATA, buffer)
+c.setopt(pycurl.VERBOSE, args.verbose)
 c.perform()
+response_info = {"code": c.getinfo(pycurl.RESPONSE_CODE),
+                 "type": c.getinfo(pycurl.CONTENT_TYPE)}
+ct_params = cidict(parse_header(response_info["type"])[1]) if response_info["type"] is not None else cidict()
+response_info["charset"] = ct_params["charset"] if "charset" in ct_params else "UTF-8"
 c.close()
 
-body = buffer.getvalue()
-print(body)
+if args.body:
+    print(re.search(args.body, buffer.getvalue().decode(response_info["charset"]), re.I))
